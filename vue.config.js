271856881c13435129aaee51a2c67262586f34d5
lesliebinbin <lesliebinbin19900129@gmail.com>
2023-08-03 10:33:01 +1000

    add gitlab config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath:
+    process.env.NODE_END == "production"
+      ? "/" + process.env.CI_PROJECT_NAME + "/"
+      : "/",
   chainWebpack(config) {
     config.resolve.alias.set("vue", "@vue/compat");
-
     config.module
       .rule("vue")
       .use("vue-loader")
